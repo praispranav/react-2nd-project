06f484bf25509077a9b2797948b8a7c79c0a8249
praispranav <praispranav@gmail.com>
2021-02-26 12:55:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>To do App</h1>
+      <div className="todoapp">
+        <input type="checkbox" />
+        <p>This is checkbox 1</p>
+
+        <input type="checkbox" />
+        <p>This is checkbox 2</p>
+        <input type="checkbox" />
+        <p>This is checkbox 3</p>
+        <input type="checkbox" />
+        <p>This is checkbox 4</p>
+      </div>
+    </div>
+  );
+}
